f3eb1b36e71c3d1cc23c1f61151bf155eface326
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-11-26 16:23:55 +0100

    Don't reset MCU every time, use interrupt handler

---
+++ README.md
@@ -0,0 +1,8 @@
+# Blinkled
+
+Blink a LED every 4 seconds on an ATTiny.
+
+See also:
+
+* [Ultra low power LED | Hackaday.io](https://hackaday.io/project/169882-ultra-low-power-led)
+* [IowaDave/ATTiny85\_Battery\_Sleep: Blink an LED for two weeks on two AAA batteries](https://github.com/IowaDave/ATTiny85_Battery_Sleep)
--- blinkled/blinkled.ino
+++ blinkled/blinkled.ino
@@ -2,21 +2,30 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/wdt.h>
-#include <avr/adc.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-int main(void) {
+void setup() {
   ADCSRA &= ~ bit(ADEN); // disable the ADC
   power_all_disable();
   wdt_enable(WDTO_4S);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 
   DDRB = 0xff;
+}
+
+void loop() {
+  WDTCR |= (1 << WDIE);
+  sleep_enable();
+  sleep_mode();
+}
+
+ISR (WDT_vect) {
+  // clear the Sleep Enable bit immediately after waking up
+  MCUCR &= ~(1 << SE);
+
+  // blink led
   PORTB = 0xff;
   _delay_ms(2);
   PORTB = 0x00;
-
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_enable();
-  sleep_mode();
 }
